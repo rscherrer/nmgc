50df0083140503f128816fc2e6ca0b1b4aa6175d
rscherrer <raphael.scherrer@evobio.eu>
2020-05-09 16:53:03 +0200

    tibbles dont like empty colnames

--- R/add_signif.R
+++ R/add_signif.R
@@ -6,22 +6,19 @@
 #' @param pcol Name of the column containing P-values
 #' @param thresholds A vector of P-value thresholds, in decreasing order
 #' @param labels A vector of labels, one for each threshold
-#' @param colname Optional name of the significance label column. Defaults to none
 #'
 #' @return A data frame
 #'
 #' @export
 
 add_signif <- function(
-  df, pcol = "pvalue", thresholds = c(0.05, 0.01, 0.001), labels = c("*", "**", "***"),
-  colname = ""
+  df, pcol = "pvalue", thresholds = c(0.05, 0.01, 0.001), labels = c("*", "**", "***")
 ) {
 
   df$signif <- rep("", nrow(df))
   for (i in seq_along(thresholds)) {
-    df <- df %>% mutate(signif = ifelse(get(pcol) < thresholds[i], labels[i], signif))
+    df <- df %>% dplyr::mutate(signif = ifelse(get(pcol) < thresholds[i], labels[i], signif))
   }
-  colnames(df)[colnames(df) == "signif"] <- colname
   return (df)
 
 }
